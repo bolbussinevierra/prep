4eb0e0f636f3d485185c9304d165aec3afbf9744
bolbussinevierra <bolbus_sinevierra@yahoo.com>
2014-01-25 14:38:49 -0800

    rename_values

--- cci/cpp/7.math.and.probability.h
+++ cci/cpp/7.math.and.probability.h
@@ -1,2 +1,14 @@
 #pragma once
+//
+// 7.4
+//
+int _negate(int a) {
+    int neg = 0;
+    int sign_delta = a < 0 ? 1 : -1;
 
+    while (a != 0) {
+        neg += sign_delta;
+        a += sign_delta;
+    }
+    return neg;
+}
