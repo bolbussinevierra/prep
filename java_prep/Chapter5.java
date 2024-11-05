118578e45550d95e87e7b090dcf2cff61b4d54e7
bolbussinevierra <bolbus_sinevierra@yahoo.com>
2024-11-05 15:54:01 -0800

    5.6, 6.1

--- java_prep/Chapter5.java
+++ java_prep/Chapter5.java
@@ -9,7 +9,12 @@ public class Chapter5 {
     public static void main(String[] args) {
         // 5.1
         ArrayList<Integer> A = new ArrayList<>(Arrays.asList(3, 4, 1, 0, -1));
-        System.out.println(dutchFlags(2, A));
+        System.out.println("5.1: -> " + dutchFlags(2, A));
+
+        ArrayList<Double> B
+                = new ArrayList<>(Arrays.asList(310.0, 315.0, 275.0, 295.0, 260.0, 270.0, 290.0, 230.0, 255.0, 250.0));
+        System.out.println("5.6: -> " + computeMaxProfit(B));
+
     }
 
     public static List<Integer> dutchFlags(int pivotIndex, List<Integer> A) {
@@ -25,5 +30,14 @@ public class Chapter5 {
             }
         }
         return A;
+    }
+
+    public static Double computeMaxProfit(List<Double> prices) {
+        double minPrice = Double.MAX_VALUE, maxProfit = 0.0;
+        for (Double p : prices) {
+            maxProfit = Math.max(maxProfit, p - minPrice);
+            minPrice = Math.min(minPrice, p);
+        }
+        return maxProfit;
     }
 }
---
+++ java_prep/Chapter6.java
@@ -0,0 +1,33 @@
+package java_prep;
+
+public class Chapter6 {
+    public static void main(String[] args) {
+        // 6.1
+        System.out.println("6.1 (intToString) -> " + intToString(-341));
+        System.out.println("6.1 (stringToInt) -> " + stringToInt("-341s"));
+
+    }
+
+    public static String intToString(int x) {
+        boolean isNegative = x < 0;
+        StringBuilder s = new StringBuilder();
+        x = Math.abs(x);
+        do {
+            s.append((char) ('0' + x % 10));
+            x /= 10;
+        } while (x != 0);
+        return s.append(isNegative ? "-" : "").reverse().toString();
+    }
+
+    public static int stringToInt(String x) {
+        return (x.charAt(0) == '-' ? -1 : 1) *
+                x.substring(x.charAt(0) == '-' || x.charAt(0) == '+' ? 1 : 0)
+                        .chars()
+                        .reduce(0, (rSum, c) -> {
+                            if (c - '0' > 9) {
+                                throw new RuntimeException();
+                            }
+                            return rSum * 10 + c - '0';
+                        });
+    }
+}
