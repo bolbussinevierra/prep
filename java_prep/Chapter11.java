1f998ed8462bafa4feeefc119d379bf3960a9c3e
bolbussinevierra <bolbus_sinevierra@yahoo.com>
2024-11-12 17:18:35 -0800

    11.1

--- java_prep/Chapter11.java
+++ java_prep/Chapter11.java
@@ -3,10 +3,13 @@ package java_prep;
 public class Chapter11 {
     public static void main(String[] args) {
         int[] A = new int[]{1, 3, 5, 6, 7, 9};
-        System.out.println(bsearch(6, A));
+        System.out.println(bSearch(6, A));
+
+        int[] B = new int[]{1, 3, 3, 3, 5, 6, 7, 9};
+        System.out.println("11.1 -> " + bSearchFirst(3, A));
     }
 
-    public static int bsearch(int t, int[] A) {
+    public static int bSearch(int t, int[] A) {
         int l = 0, u = A.length - 1;
         while (l <= u) {
             int m = (l + u) / 2;
@@ -19,5 +22,23 @@ public class Chapter11 {
             }
         }
         return -1;
+    }
+
+    // 11.1
+    public static int bSearchFirst(int t, int [] A) {
+        int l = 0, r = A.length - 1, result = -1;
+
+        while (l <= r) {
+            int m = l + ((r - l) / 2);
+            if (A[m] < t) {
+                l = m + 1;
+            } else if (A[m] == t) {
+                result = m;
+                r = m - 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return result;
     }
 }
