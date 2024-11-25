a0a531e304928048f12c16135e85c8a303a8020a
bolbussinevierra <bolbus_sinevierra@yahoo.com>
2024-11-25 14:19:06 -0800

    6.2

--- java_prep/Chapter6.java
+++ java_prep/Chapter6.java
@@ -6,6 +6,9 @@ public class Chapter6 {
         System.out.println("6.1 (intToString) -> " + intToString(-341));
         System.out.println("6.1 (stringToInt) -> " + stringToInt("-341"));
 
+        // 6.2
+        System.out.println("6.2 -> " + convertBase("615", 7, 13));
+
     }
 
     public static String intToString(int x) {
@@ -30,4 +33,24 @@ public class Chapter6 {
                             return rSum * 10 + c - '0';
                         });
     }
+
+    // 6.2
+    public static String convertBase(String num, int b1, int b2) {
+        boolean isNegative = num.startsWith("-");
+        int numDec = num.substring(isNegative ? 1 : 0)
+                .chars()
+                .reduce(0, (x, c) -> x * b1 + (Character.isDigit(c) ? c - '0' : c - 'A' + 10));
+        if (numDec == 0) return "0";
+
+        return (isNegative ? "-" : "") + constructFromBase(numDec, b2);
+
+    }
+
+    public static String constructFromBase(int num, int b) {
+        if (num == 0) return "";
+        int rem = num % b;
+        return constructFromBase(num / b, b) +
+                (char) (rem >= 10 ? 'A' + rem - 10 : '0' + rem);
+    }
+
 }
