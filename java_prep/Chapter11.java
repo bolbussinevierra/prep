710abba23d8d9ab191ff3e8b3d128717c6ef887d
bolbussinevierra <bolbus_sinevierra@yahoo.com>
2024-11-26 16:27:16 -0800

    11.4

--- java_prep/Chapter11.java
+++ java_prep/Chapter11.java
@@ -5,8 +5,12 @@ public class Chapter11 {
         int[] A = new int[]{1, 3, 5, 6, 7, 9};
         System.out.println(bSearch(6, A));
 
+        // 11.1
         int[] B = new int[]{1, 3, 3, 3, 5, 6, 7, 9};
         System.out.println("11.1 -> " + bSearchFirst(3, B));
+
+        // 11.4
+        System.out.println("11.4 -> " + integerSquareRoot(300));
     }
 
     public static int bSearch(int t, int[] A) {
@@ -25,7 +29,7 @@ public class Chapter11 {
     }
 
     // 11.1
-    public static int bSearchFirst(int t, int [] A) {
+    public static int bSearchFirst(int t, int[] A) {
         int l = 0, r = A.length - 1, result = -1;
 
         while (l <= r) {
@@ -40,5 +44,21 @@ public class Chapter11 {
             }
         }
         return result;
+    }
+
+    // 11.4
+    public static int integerSquareRoot(int n) {
+        long left = 0, right = n;
+
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            long midSquared = mid * mid;
+            if (midSquared <= n) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return (int) left - 1;
     }
 }
