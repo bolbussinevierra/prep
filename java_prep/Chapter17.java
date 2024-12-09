f0d56f3b178962bcfd677e39402d895d8f407fb9
bolbussinevierra <bolbus_sinevierra@yahoo.com>
2024-12-09 15:41:35 -0800

    17 two sum sorted

--- java_prep/Chapter17.java
+++ java_prep/Chapter17.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Chapter17 {
     public static void main(String[] args) {
         System.out.println("TwoSum -> " + hasTwoSum(List.of(1,5,7,9), 14));
+        System.out.println("TwoSumSorted -> " + hasTwoSumSorted(List.of(1,5,7,9), 14));
 
         // 17.4
         System.out.println("17.4 -> " + hasThreeSum(List.of(1, 5, 7, 9), 15));
@@ -19,6 +20,21 @@ public class Chapter17 {
                 return true;
             }
             prev.add(i);
+        }
+        return false;
+    }
+
+    private static boolean hasTwoSumSorted(List<Integer> a, int s) {
+        int l = 0, r = a.size() - 1;
+        while (l < r) {
+            int currentSum = a.get(l) + a.get(r);
+            if (currentSum == s) {
+                return true;
+            } else if (currentSum > s) {
+                --r;
+            } else {
+                ++l;
+            }
         }
         return false;
     }
