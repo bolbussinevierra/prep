de3cdec458902026e1b27dd8ca1c1702227f784f
bolbussinevierra <bolbus_sinevierra@yahoo.com>
2024-12-13 17:03:53 -0800

    7.4

--- java_prep/Chapter7.java
+++ java_prep/Chapter7.java
@@ -11,7 +11,51 @@ public class Chapter7 {
         ListNode<Integer> L1 = makeLinkedList(new int[]{1, 4, 6});
         ListNode<Integer> L2 = makeLinkedList(new int[]{2, 3, 5});
         ListNode<Integer> sorted = mergeSorted(L1, L2);
-        System.out.println(sorted);
+        System.out.println("7.1 -> " + sorted);
+
+        // 7.4
+        ListNode<Integer> list1 = makeLinkedList(new int[]{1, 2, 3, 4, 5});
+        ListNode<Integer> list2 = makeLinkedList(new int[]{1, 2, 3});
+        ListNode<Integer> tmp1 = list1;
+        ListNode<Integer> tmp2 = list2;
+        while (tmp1.data != 4) tmp1 = tmp1.next;
+        while (tmp2.data != 3) tmp2 = tmp2.next;
+        tmp2.next = tmp1;
+        System.out.println("7.4 -> " + firstOverlappingNode(list1, list2).data);
+    }
+
+    public static ListNode<Integer> firstOverlappingNode(
+            ListNode<Integer> listA,
+            ListNode<Integer> listB) {
+        // Determine the length and advance the longer one by the difference.
+        int lengthA = getLength(listA), lengthB = getLength(listB);
+        if (lengthA > lengthB) {
+            listA = advanceListBy(lengthA - lengthB, listA);
+        } else {
+            listB = advanceListBy(lengthB - lengthA, listB);
+        }
+
+        while (listA != null && listB != null && listA != listB) {
+            listA = listA.next;
+            listB = listB.next;
+        }
+        return listA; // nullptr implies no overlap;
+    }
+
+    static int getLength(ListNode<Integer> l) {
+        int length = 0;
+        while (l != null) {
+            ++length;
+            l = l.next;
+        }
+        return length;
+    }
+
+    static ListNode<Integer> advanceListBy(int k, ListNode<Integer> l) {
+        while (k-- > 0) {
+            l = l.next;
+        }
+        return l;
     }
 
     static ListNode<Integer> mergeSorted(ListNode<Integer> L1,
