61e0465c8bda70fe9e89304d96ceb78b8b64493a
bolbussinevierra <bolbus_sinevierra@yahoo.com>
2024-12-23 16:27:15 -0800

    18.2

--- java_prep/Chapter18.java
+++ java_prep/Chapter18.java
@@ -3,84 +3,121 @@ package java_prep;
 import java.util.*;
 
 public class Chapter18 {
-    public static void main(String[] args) {
-        // 18.1
-        searchMaze(List.of(), new Coordinate(0, 0), new Coordinate(0, 0));
+  public static void main(String[] args) {
+    // 18.1
+    searchMaze(List.of(), new Coordinate(0, 0), new Coordinate(0, 0));
 
-        // 18.7
-        Set<String> D = new HashSet<>(List.of("bat", "cot", "dog", "dag", "dot", "cat"));
-        String s = "cat";
-        String t = "dog";
-        System.out.println("18.7 -> " + transformString(D, s, t));
+    // 18.2
+    flipColor(0, 0, List.of(new ArrayList<>(List.of(Boolean.FALSE))));
+
+    // 18.7
+    Set<String> D = new HashSet<>(List.of("bat", "cot", "dog", "dag", "dot", "cat"));
+    String s = "cat";
+    String t = "dog";
+    System.out.println("18.7 -> " + transformString(D, s, t));
+  }
+
+  public static List<Coordinate> searchMaze(List<List<Color>> maze, Coordinate s, Coordinate e) {
+    List<Coordinate> path = new ArrayList<>();
+    searchMazeHelper(maze, s, e, path);
+    return path;
+  }
+
+  private static int transformString(Set<String> D, String s, String t) {
+    Set<String> unvisited = new HashSet<>(D);
+    Queue<StringDistance> q = new ArrayDeque<>();
+    unvisited.remove(s);
+    q.add(new StringDistance(s, 0));
+
+    StringDistance f;
+    while ((f = q.poll()) != null) {
+      // Are we done?
+      if (f.candidate().equals(t)) {
+        return f.distance();
+      }
+
+      // Try all children that are 1 character transformation away
+      String str = f.candidate();
+      for (int i = 0; i < str.length(); ++i) {
+        String strStart = i == 0 ? "" : str.substring(0, i);
+        String strEnd = i + 1 < str.length() ? str.substring(i + 1) : "";
+        for (char c = 'a'; c <= 'z'; ++c) {
+          String newCandidate = strStart + c + strEnd;
+          if (unvisited.contains(newCandidate)) {
+            unvisited.remove(newCandidate);
+            q.add(new StringDistance(newCandidate, f.distance() + 1));
+          }
+        }
+      }
+    }
+    return -1; // Cannot find a transformation.
+  }
+
+  // Perform a DFS to find a feasible path.
+  private static boolean searchMazeHelper(
+      List<List<Color>> maze, Coordinate c, Coordinate e, List<Coordinate> path) {
+    // Ensure within maze and white cell
+    if (c.x < 0
+        || c.x >= maze.size()
+        || c.y < 0
+        || c.y >= maze.get(c.x).size()
+        || maze.get(c.x).get(c.y) != Color.WHITE) {
+      return false;
+    }
+    path.add(c);
+    maze.get(c.x).set(c.y, Color.BLACK);
+    if (c.equals(e)) {
+      return true;
     }
 
-    private record Coordinate(int x, int y) {}
-    private record StringDistance(String candidate, Integer distance) {}
-
-    public enum Color {WHITE, BLACK}
-
-    public static List<Coordinate> searchMaze(List<List<Color>> maze,
-                                              Coordinate s, Coordinate e) {
-        List<Coordinate> path = new ArrayList<>();
-        searchMazeHelper(maze, s, e, path);
-        return path;
+    for (Coordinate nextMove :
+        List.of(
+            new Coordinate(c.x, c.y + 1),
+            new Coordinate(c.x, c.y - 1),
+            new Coordinate(c.x + 1, c.y),
+            new Coordinate(c.x - 1, c.y))) {
+      if (searchMazeHelper(maze, nextMove, e, path)) {
+        return true;
+      }
     }
+    // Cannot find a path, remove the entry added to path.
+    path.removeLast();
+    return false;
+  }
 
-    private static int transformString(Set<String> D, String s, String t) {
-        Set<String> unvisited = new HashSet<>(D);
-        Queue<StringDistance> q = new ArrayDeque<>();
-        unvisited.remove(s);
-        q.add(new StringDistance(s, 0));
+  public static void flipColor(int x, int y, List<List<Boolean>> image) {
+    boolean color = image.get(x).get(y);
 
-        StringDistance f;
-        while ((f = q.poll()) != null) {
-            // Are we done?
-            if (f.candidate().equals(t)) {
-                return f.distance();
-            }
+    Queue<Coordinate> q = new ArrayDeque<>();
+    image.get(x).set(y, !color); // Flips
+    q.add(new Coordinate(x, y));
 
-            // Try all children that are 1 character transformation away
-            String str = f.candidate();
-            for (int i = 0; i < str.length(); ++i) {
-                String strStart = i == 0 ? "" : str.substring(0, i);
-                String strEnd = i + 1 < str.length() ? str.substring(i + 1): "";
-                for (char c = 'a'; c <= 'z'; ++c) {
-                    String newCandidate = strStart + c + strEnd;
-                    if (unvisited.contains(newCandidate)) {
-                        unvisited.remove(newCandidate);
-                        q.add(new StringDistance(newCandidate, f.distance() + 1));
-                    }
-                }
-            }
+    while (!q.isEmpty()) {
+      Coordinate c = q.remove();
+      for (Coordinate next :
+          List.of(
+              new Coordinate(c.x, c.y + 1),
+              new Coordinate(c.x, c.y - 1),
+              new Coordinate(c.x + 1, c.y),
+              new Coordinate(c.x - 1, c.y))) {
+        if (next.x >= 0
+            && next.x < image.size()
+            && next.y >= 0
+            && next.y < image.get(next.x).size()
+            && image.get(next.x).get(next.y) == color) {
+          image.get(next.x).set(next.y, !color);
+          q.add(next);
         }
-        return -1; // Cannot find a transformation.
+      }
     }
+  }
 
-    // Perform a DFS to find a feasible path.
-    private static boolean searchMazeHelper(List<List<Color>> maze, Coordinate c,
-                                            Coordinate e, List<Coordinate> path) {
-        // Ensure within maze and white cell
-        if (c.x < 0 || c.x >= maze.size()
-                || c.y < 0 || c.y >= maze.get(c.x).size() ||
-                maze.get(c.x).get(c.y) != Color.WHITE) {
-            return false;
-        }
-        path.add(c);
-        maze.get(c.x).set(c.y, Color.BLACK);
-        if (c.equals(e)) {
-            return true;
-        }
+  public enum Color {
+    WHITE,
+    BLACK
+  }
 
-        for (Coordinate nextMove : List.of(new Coordinate(c.x, c.y + 1),
-                new Coordinate(c.x, c.y - 1),
-                new Coordinate(c.x + 1, c.y),
-                new Coordinate(c.x - 1, c.y))) {
-            if (searchMazeHelper(maze, nextMove, e, path)) {
-                return true;
-            }
-        }
-        // Cannot find a path, remove the entry added to path.
-        path.removeLast();
-        return false;
-    }
+  public record Coordinate(int x, int y) {}
+
+  private record StringDistance(String candidate, Integer distance) {}
 }
