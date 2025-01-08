138bd5a2a047536b8f90dbe3c4ff2bf6ee12124d
bolbussinevierra <bolbus_sinevierra@yahoo.com>
2025-01-08 14:58:34 -0800

    18.3

--- java_prep/Chapter18.java
+++ java_prep/Chapter18.java
@@ -10,6 +10,20 @@ public class Chapter18 {
     // 18.2
     flipColor(0, 0, List.of(new ArrayList<>(List.of(Boolean.FALSE))));
 
+    // 18.3
+    List<List<Character>> board =
+        List.of(
+            Arrays.asList('B', 'B', 'B', 'B'),
+            Arrays.asList('W', 'B', 'W', 'B'),
+            Arrays.asList('B', 'W', 'W', 'B'),
+            Arrays.asList('B', 'B', 'B', 'B'));
+    fillSorroundedRegions(board);
+    System.out.println("--- 18.3 ----" );
+    for (int row = 0; row < board.size(); ++row) {
+      System.out.println(board.get(row));
+    }
+    System.out.println("-------------" );
+
     // 18.7
     Set<String> D = new HashSet<>(List.of("bat", "cot", "dog", "dag", "dot", "cat"));
     String s = "cat";
@@ -17,13 +31,85 @@ public class Chapter18 {
     System.out.println("18.7 -> " + transformString(D, s, t));
   }
 
+  // 18.1
   public static List<Coordinate> searchMaze(List<List<Color>> maze, Coordinate s, Coordinate e) {
     List<Coordinate> path = new ArrayList<>();
     searchMazeHelper(maze, s, e, path);
     return path;
   }
 
-  private static int transformString(Set<String> D, String s, String t) {
+  // 18.2
+  public static void flipColor(int x, int y, List<List<Boolean>> image) {
+    boolean color = image.get(x).get(y);
+
+    Queue<Coordinate> q = new ArrayDeque<>();
+    image.get(x).set(y, !color); // Flips
+    q.add(new Coordinate(x, y));
+
+    while (!q.isEmpty()) {
+      Coordinate c = q.remove();
+      for (Coordinate next :
+          List.of(
+              new Coordinate(c.x, c.y + 1),
+              new Coordinate(c.x, c.y - 1),
+              new Coordinate(c.x + 1, c.y),
+              new Coordinate(c.x - 1, c.y))) {
+        if (next.x >= 0
+            && next.x < image.size()
+            && next.y >= 0
+            && next.y < image.get(next.x).size()
+            && image.get(next.x).get(next.y) == color) {
+          image.get(next.x).set(next.y, !color);
+          q.add(next);
+        }
+      }
+    }
+  }
+
+  // 18.3
+  public static void fillSorroundedRegions(List<List<Character>> board) {
+    // Identify regions reachable via white path starting from the first or last columns.
+    for (int row = 0; row < board.size(); ++row) {
+      markBoundaryRegion(row, /* col= */ 0, board);
+      markBoundaryRegion(row, /* col= */ board.get(row).size() - 1, board);
+    }
+
+    // Identify regions reachable via white path starting from the first or last rows
+    for (int col = 0; col < board.getFirst().size(); ++col) {
+      markBoundaryRegion(/* row= */ 0, col, board);
+      markBoundaryRegion(/* row= */ board.size() - 1, col, board);
+    }
+
+    // Mark the unreachable regions as black.
+    for (int row = 0; row < board.size(); ++row) {
+      for (int col = 0; col < board.get(row).size(); ++col) {
+        board.get(row).set(col, board.get(row).get(col) != 'R' ? 'B' : 'W');
+      }
+    }
+  }
+
+  private static void markBoundaryRegion(int row, int col, List<List<Character>> board) {
+    Queue<Coordinate> q = new ArrayDeque<>();
+    q.add(new Coordinate(row, col));
+    // Uses BFS to traverse this region.
+    while (!q.isEmpty()) {
+      Coordinate curr = q.poll();
+      if (curr.x >= 0
+          && curr.x < board.size()
+          && curr.y >= 0
+          && curr.y < board.get(curr.x).size()
+          && board.get(curr.x).get(curr.y) == 'W') {
+        board.get(curr.x).set(curr.y, 'R');
+        q.add(new Coordinate(curr.x - 1, curr.y));
+        q.add(new Coordinate(curr.x + 1, curr.y));
+        q.add(new Coordinate(curr.x, curr.y - 1));
+        q.add(new Coordinate(curr.x, curr.y + 1));
+      }
+    }
+  }
+
+  // 18.7
+  public static int transformString(Set<String> D, String s, String t) {
     Set<String> unvisited = new HashSet<>(D);
     Queue<StringDistance> q = new ArrayDeque<>();
     unvisited.remove(s);
@@ -83,33 +169,6 @@ public class Chapter18 {
     // Cannot find a path, remove the entry added to path.
     path.removeLast();
     return false;
-  }
-
-  public static void flipColor(int x, int y, List<List<Boolean>> image) {
-    boolean color = image.get(x).get(y);
-
-    Queue<Coordinate> q = new ArrayDeque<>();
-    image.get(x).set(y, !color); // Flips
-    q.add(new Coordinate(x, y));
-
-    while (!q.isEmpty()) {
-      Coordinate c = q.remove();
-      for (Coordinate next :
-          List.of(
-              new Coordinate(c.x, c.y + 1),
-              new Coordinate(c.x, c.y - 1),
-              new Coordinate(c.x + 1, c.y),
-              new Coordinate(c.x - 1, c.y))) {
-        if (next.x >= 0
-            && next.x < image.size()
-            && next.y >= 0
-            && next.y < image.get(next.x).size()
-            && image.get(next.x).get(next.y) == color) {
-          image.get(next.x).set(next.y, !color);
-          q.add(next);
-        }
-      }
-    }
   }
 
   public enum Color {
