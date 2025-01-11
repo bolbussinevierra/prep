65f733669245840c018bae3f86b94e79509f3028
bolbussinevierra <bolbus_sinevierra@yahoo.com>
2025-01-10 16:44:49 -0800

    5.15

--- java_prep/Chapter5.java
+++ java_prep/Chapter5.java
@@ -38,6 +38,9 @@ public class Chapter5 {
     randomSampling(3, arr3);
     System.out.println("5.12 -> " + arr3);
 
+    // 5.15
+    System.out.println("5.15 -> " + randomSubset(10, 5));
+
     // 5.17
     ArrayList<List<Integer>> sudoku =
         new ArrayList<>() {
@@ -192,6 +195,38 @@ public class Chapter5 {
     for (int i = 0; i < k; ++i) {
       Collections.swap(arr, i, i + random.nextInt(arr.size() - i));
     }
+  }
+
+  // 5.15
+  public static List<Integer> randomSubset(int n, int k) {
+    Map<Integer, Integer> changedElements = new HashMap<>();
+    Random randIdxGen = new Random();
+    for (int i = 0; i < k; ++i) {
+      // Generate random int in [i, n - 1].
+      int randIdx = i + randIdxGen.nextInt(n - i);
+      Integer ptr1 = changedElements.get(randIdx);
+      Integer ptr2 = changedElements.get(i);
+
+      if (ptr1 == null && ptr2 == null) {
+        changedElements.put(randIdx, i);
+        changedElements.put(i, randIdx);
+      } else if (ptr1 == null && ptr2 != null) {
+        changedElements.put(randIdx, ptr2);
+        changedElements.put(i, randIdx);
+      } else if (ptr1 != null && ptr2 == null) {
+        changedElements.put(i, ptr1);
+        changedElements.put(randIdx, i);
+      } else {
+        changedElements.put(i, ptr1);
+        changedElements.put(randIdx, ptr2);
+      }
+    }
+
+    List<Integer> result = new ArrayList<>(k);
+    for (int i = 0; i < k; ++i) {
+      result.add(changedElements.get(i));
+    }
+    return result;
   }
 
   // 5.18 (uses neet code Spiral Matrix II solution)
