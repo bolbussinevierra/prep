4938582af234583e65f93baa4a61febda474e3c8
bolbussinevierra <bolbus_sinevierra@yahoo.com>
2025-03-06 17:03:55 -0800

    24.2

--- java_prep/Chapter24.java
+++ java_prep/Chapter24.java
@@ -1,9 +1,17 @@
 package java_prep;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class Chapter24 {
     public static void main(String[] args){
         // 24.1
         System.out.println("24.1 -> " + GCD(24, 300));
+
+        // 24.2
+        System.out.println("24.2 -> " + findFirstMissingPositive(Arrays.asList(3, 4, 0, 2)));
     }
 
     // 24.1
@@ -24,5 +32,20 @@ public class Chapter24 {
         } else {  // both are odd
             return GCD(x, y - x);
         }
+    }
+
+    // 24.2
+    public static int findFirstMissingPositive(List<Integer> A) {
+        for (int i = 0; i < A.size(); ++i) {
+            while (0 < A.get(i) && A.get(i) <= A.size()
+                           && !A.get(i).equals(A.get(A.get(i) - 1))) {
+                Collections.swap(A, i, A.get(i) - 1);
+            }
+        }
+
+        return IntStream.range(0, A.size())
+                       .filter(i -> A.get(i) != i + 1)
+                       .findFirst()
+                       .orElse(A.size()) + 1;
     }
 }
